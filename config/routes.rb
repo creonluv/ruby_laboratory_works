59b4a455917284cd869a3ba6f230cce06aa1fa2d
Dmytro Berezhnyi <Dmytro Berezhnyi>
2024-03-03 13:59:47 +0200

    Create user and category scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :users
   resources :roles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
