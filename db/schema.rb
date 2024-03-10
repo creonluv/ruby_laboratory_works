cc774fb1ab3fe25a90c3bcf4a75edef4bd38a038
Ivan <tixehack@gmail.com>
2024-03-10 16:14:12 +0200

    add styles for main page and refactor add forms

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_122314) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "Write content"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
